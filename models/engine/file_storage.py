832ffa44954bd3cc7564c6314819f84e3c61b35c
Samuel <muchokisamuel@gmail.com>
2023-12-11 15:31:19 +0000

    Console 1.0

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,18 +44,17 @@ class FileStorage:
 
         from models.base_model import BaseModel
         from models.user import User
-        # from models.place import Place
-        # from models.state import State
-        # from models.city import City
-        # from models.amenity import Amenity
-        # from models.review import Review
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-        classes = {'BaseModel': BaseModel, 'User': User}
-        """classes = {
+        classes = {
                     'BaseModel' : BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
-                   }"""
+                   }
 
         if os.path.exists(FileStorage.__file_path):
             obj_dict = {}
